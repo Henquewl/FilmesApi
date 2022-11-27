adaec2223eb855fb259f1eea54113df87e0b494e
Henrique Wolff Lobo <henquewl@hotmail.com>
2022-11-27 20:07:16 -0300

    Criado services para tirar a responsabilidade de lógica por parte dos controladores.

--- FilmesApi/Startup.cs
+++ FilmesApi/Startup.cs
@@ -1,5 +1,7 @@
 using FilmesApi.Data;
+using FilmesApi.Services;
 using FilmesAPI.Data;
+using GerentesApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +34,11 @@ namespace FilmesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(opts => opts.UseLazyLoadingProxies().UseMySQL(Configuration.GetConnectionString("CinemaConnection")));
+            services.AddScoped<FilmeService, FilmeService>();
+            services.AddScoped<CinemaService, CinemaService>();
+            services.AddScoped<EnderecoService, EnderecoService>();
+            services.AddScoped<GerenteService, GerenteService>();
+            services.AddScoped<SessaoService, SessaoService>();
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
